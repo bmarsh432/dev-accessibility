758dcd82cf47410bebd86199c6407053ffbc9f2f
Ben Marsh <ben.marsh@kpmg.co.uk>
2022-01-04 16:58:20 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import smiley from "./images/smiley.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>How can we catch Accessibility issues</h1>
+      <div>Here are some html elements which have some issues when it comes to acccessibility</div>
+      <div>
+        <h3>Button</h3>
+        <Button></Button>
+      </div>
+      <div>
+        <h3>Image</h3>
+        <img src={smiley}></img>
+      </div>
+      <div>
+        <h3>Input</h3>
+        <Input type="text" labelText="Name" id="name" />
+      </div>
+      <div>
+        <h3>Link</h3>
+        <span
+          onClick={() => console.log("Link clicked")}
+          // role="button"
+          // tabIndex={0}
+          // onKeyDown={() => console.log("Link clicked")}
         >
-          Learn React
-        </a>
-      </header>
+          This is a link
+        </span>
+      </div>
+      <div>
+        <h3>Contrast</h3>
+        <span style={{ color: "lightgray" }}>The quick brown fox jumps over the lazy dog</span>
+      </div>
     </div>
   );
 }
