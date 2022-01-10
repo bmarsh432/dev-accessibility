1b7e25d55660b6e888567f92c0285639f49790e4
Ben Marsh <ben.marsh@kpmg.co.uk>
2022-01-10 11:36:34 +0000

    simplified components, added lighthouse ci

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
 import smiley from "./images/smiley.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>How can we catch Accessibility issues</h1>
+      <h1>How can we catch Accessibility issues?</h1>
       <div>Here are some html elements which have some issues when it comes to acccessibility</div>
       <div>
         <h3>Button</h3>
-        <Button></Button>
+        <button type="button"></button>
       </div>
       <div>
         <h3>Image</h3>
@@ -18,24 +16,49 @@ function App() {
       </div>
       <div>
         <h3>Input</h3>
-        <Input type="text" labelText="Name" id="name" />
+        <input type="text" />
       </div>
       <div>
         <h3>Link</h3>
-        <span
-          onClick={() => console.log("Link clicked")}
-          // role="button"
-          // tabIndex={0}
-          // onKeyDown={() => console.log("Link clicked")}
-        >
-          This is a link
-        </span>
+        <span onClick={() => console.log("Link clicked")}>This is a link</span>
       </div>
       <div>
-        <h3>Contrast</h3>
+        <h3>Text</h3>
         <span style={{ color: "lightgray" }}>The quick brown fox jumps over the lazy dog</span>
       </div>
     </div>
+    // <main className="App">
+    //   <h1>How can we catch Accessibility issues</h1>
+    //   <div>Here are some html elements which have some issues when it comes to acccessibility</div>
+    //   <div>
+    //     <h2>Input</h2>
+    //     <button type="button">Click me...</button>
+    //   </div>
+    //   <div>
+    //     <h2>Input</h2>
+    //     <img src={smiley} alt="smiley"></img>
+    //   </div>
+    //   <div>
+    //     <h2>Input</h2>
+    //     <label htmlFor="name">Name</label>
+    //     <input name="name" id="name" type="text" />
+    //   </div>
+    //   <div>
+    //     <h2>Link</h2>
+    //     <span
+    //       onClick={() => console.log("Link clicked")}
+    //       role="button"
+    //       tabIndex={0}
+    //       onKeyDown={() => console.log("Link clicked")}
+    //     >
+    //       This is a link
+    //     </span>
+    //   </div>
+    //   <div>
+    //     <h2>Contrast</h2>
+    //     <span style={{ color: "black" }}>The quick brown fox jumps over the lazy dog</span>
+    //   </div>
+    // </main>
   );
 }
 
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface ButtonProps {
-  children?: any;
-}
-
-export const Button: React.FC<ButtonProps> = ({ children }: ButtonProps) => {
-  return <button type="button">{children}</button>;
-};
